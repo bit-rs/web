14ba5634878fae874f1bb828c7116c0c6d354e76
vyacheslavhere <slavlendstudio@gmail.com>
2026-02-26 12:01:41 +0500

    first commit

---
+++ posts.js
@@ -0,0 +1,19 @@
+const POSTS = [
+  {
+    slug: "test",
+    meta: {
+      title: "🎯 Test post is here",
+      date: "Feb 11, 2026",
+      author: "@vyacheslavhere",
+      category: "devlog",
+      excerpt: "Test excerpt.",
+    },
+    content: `
+## Welcome! 🛰️
+
+\`\`\`
+println("Hello, world!");
+\`\`\`
+`,
+  },
+];
